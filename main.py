e313817030c8ff9eee68b9e2af01c28c39355990
Nikhil Rane <99159420+nikhilrane-21@users.noreply.github.com>
2023-11-23 00:02:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -96,26 +96,7 @@ ctx = webrtc_streamer(
       {
         "urls": "stun:stun.relay.metered.ca:80",
       },
-      {
-        "urls": "turn:a.relay.metered.ca:80",
-        "username": "9d79830e9a30b210d0582c23",
-        "credential": "6TzT7r9tBsdKHdMD",
-      },
-      {
-        "urls": "turn:a.relay.metered.ca:80?transport=tcp",
-        "username": "9d79830e9a30b210d0582c23",
-        "credential": "6TzT7r9tBsdKHdMD",
-      },
-      {
-        "urls": "turn:a.relay.metered.ca:443",
-        "username": "9d79830e9a30b210d0582c23",
-        "credential": "6TzT7r9tBsdKHdMD",
-      },
-      {
-        "urls": "turn:a.relay.metered.ca:443?transport=tcp",
-        "username": "9d79830e9a30b210d0582c23",
-        "credential": "6TzT7r9tBsdKHdMD",
-      },
+
   ]},  # Add this to config for cloud deployment.
     media_stream_constraints={"video": {"height": {"ideal": 480}}, "audio": True},
     video_html_attrs=VideoHTMLAttributes(autoPlay=True, controls=False, muted=False),
